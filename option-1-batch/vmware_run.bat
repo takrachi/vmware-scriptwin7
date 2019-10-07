f15511d21199fca52b2516ebd7b26c283587b207
takrachi <rachel_takla@hotmail.com>
2019-10-07 12:49:36 +0000

    ajout des scripts

---
+++ option-1-batch/vmware_run.bat
@@ -0,0 +1,19 @@
+@echo off
+
+rem chemin ou se trouve les fichiers .vmdk et .vmx
+set vm_path="C:\windows 7\"
+set vmdk_default=%vm_path%"windows 7-default.vmdk"
+set vmdk_current=%vm_path%"windows 7.vmdk"
+set win7vmx=%vm_path%"windows 7.vmx"
+rem chemin de l'executable vmplayer.exe
+set vmplayerexe="C:\Program Files (x86)\VMware\VMware Player\vmplayer.exe"
+
+rem ecrase le disque virtuel utilise par la vm avec le disque virtuel de defaut
+echo [*] Initialisation de la machine virtuelle. Veuillez patienter...
+echo    [+] Creation du disque virtuel.
+start /min /wait xcopy /y %vmdk_default% %vmdk_current%
+echo    [+] Lancement de la machine virtuelle.
+timeout /t 2 /nobreak > NUL
+
+rem demarre la machine virtuelle Windows 7
+start /b %vmplayerexe% %win7vmx%
