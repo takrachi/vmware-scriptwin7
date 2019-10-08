979f01fe0265eaae2ea50a50596508fc06467130
Rachel <rachel_takla@hotmail.com>
2019-10-08 17:40:27 -0400

    Update nettoyage.ps1

--- option-3-scriptbureau/nettoyage.ps1
+++ option-3-scriptbureau/nettoyage.ps1
@@ -5,5 +5,3 @@ Get-ChildItem "C:\Users\" -include *.rss -recurse | foreach { Remove-Item -Path 
 $Shell = New-Object -ComObject Shell.Application 
 $RecycleBin = $Shell.Namespace(0xA) 
 $RecycleBin.Items() | %{Remove-Item $_.Path -Recurse -Confirm:$false}
-# Si la machine virtuelle a Powershell v.5 on peut remplacer le script pour vider la corbeille par
-#Clear-RecycleBin -Force
