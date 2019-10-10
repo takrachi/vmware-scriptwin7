86c63dd5a13e9714fcfd76fc5406c9aeeedc80f8
Rachel <rachel_takla@hotmail.com>
2019-10-10 15:33:28 -0400

    Modification du type de popup

--- Nettoyage.ps1
+++ Nettoyage.ps1
@@ -1,12 +1,12 @@
 # Popup de confirmation
-$Shell_VB = New-Object -ComObject WScript.Shell
 $Titre = "Suppression des fichiers RSS"
 $Message = @("Vous êtes sur le point de supprimer définitivement vos fichiers RSS.`n`n"
             "Souhaitez-vous continuer?`n`n"
             ) -join ''
-$Decision = $Shell_VB.popup($Message, 0, $Titre, 0x4+0xF)
+Add-Type -AssemblyName System.Windows.Forms
+$Result = [System.Windows.Forms.MessageBox]::Show($Message, $Title, 'YesNo', 'Warning', "Button2")     
 
-if ($Decision -eq 6) { # Oui
+if ($Result -eq 'Yes') { # Oui
     # Cherche les fichiers d'extensions RSS et zip recursivement dans C:\Users\ puis les supprime 
     Get-ChildItem "C:\Users\" -include *.rss -recurse | foreach { Remove-Item -Path $_.FullName }
     Get-ChildItem "C:\Users\" -include *.zip -recurse | foreach { Remove-Item -Path $_.FullName }
@@ -16,8 +16,10 @@ if ($Decision -eq 6) { # Oui
     $RecycleBin = $Shell.Namespace(0xA) 
     $RecycleBin.Items() | %{ Remove-Item $_.Path -Recurse -Confirm:$false }
     
+    [System.Windows.Forms.MessageBox]::Show($Message, $Title, 'YesNo', 'Warning', "Button2") 
     $Shell_VB.popup("Vos fichiers ont été supprimés.`n", 0, "Suppression terminée")
 }
-else { # Non/Annuler
+else { # Non
+    [System.Windows.Forms.MessageBox]::Show($Message, $Title, 'YesNo', 'Warning', "Button2") 
     $Shell_VB.popup("Opération annulée.`n", 0, "Suppression annulée")
 }
